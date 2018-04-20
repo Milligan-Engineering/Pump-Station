3373dfe71bad6c898be92179c8df21017be86545
Jeffrey Giesey <jjgiesey@milligan.edu>
2018-04-20 08:22:51 -0400

    Revert "Comments on PM08"
    This reverts commit 7e9689fed4bfcd36a6a403de72e5fc194bd1a4c1.

--- Pump Station/Guage.h
+++ Pump Station/Guage.h
@@ -8,18 +8,17 @@ using namespace std;
 class Guage
 {
 public:
-	Guage(int SampSize);
 	double avgdata(double dataArray[], int arraysize);
 	// average data function
 	//void set(int SampSize, double DataArray[]);
 	double getDataArray();
 	int getSampSize();
-	// Need mutators to set values.
+	Guage(int SampSize);
 
 
 private:
 	string name;
-	double DataArray[10];
+	double DataArray[];
 	int SampSize;
 
 	
